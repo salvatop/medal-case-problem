9dac9dd0f5c9d8b91252dac948ff40dbbbceb585
Salvatore Palazzo <salvatop78@gmail.com>
2024-11-23 11:50:45 -0500

    Initial Commit

---
+++ medal-case-problem/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  medal-case-problem
+//
+//  Created by Salvatore Palazzo on 2024-11-23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
